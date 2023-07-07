58ce5dabc66b983de2e85e17b402f8b81f534c5c
MarcinNieradka <nieradka.marcin@gmail.com>
2023-07-07 18:58:52 +0200

    Update MovieDetails.jsx

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { fetchMovieDetails } from 'services';
 import { useLocation } from 'react-router-dom';
-// import { BackLink } from 'components/BackLink';
 import { Outlet } from 'react-router-dom';
 import { Suspense } from 'react';
 import { makeGenresList } from 'services/genres';
